b36512671cbb3c0006df33346f265ed620996f41
kimochg <liucheng.cn@outlook.com>
2016-01-24 23:48:26 +0800

    mod CutStock.cpp(run in buggy!), SubSetSum(rewrite!)

--- Blank Project/CutStock.cpp
+++ Blank Project/CutStock.cpp
@@ -17,34 +17,47 @@
 #include <string>
 #include "simpio.h"
 #include "vector.h"
+#include "map.h"
 
 using namespace std;
 
 /* Function prototyep */
 bool canCuted(Vector<int> & vec, int length);
-void subSetSum(Vector<int> & vec, int length, Vector<int> & result);
-// int CutStock(Vector<int> & requests, int stockLength);
+// bool isSubVec(Vector<int> subvec, Vector<int> vec);
+Map<int, Vector<int> > subSetSumVecs(Vector<int> & vec, int length);
+int CutStock(Vector<int> & requests, int stockLength);
+
 
 /* Main program */
 
 int main() 
 {
-	Vector<int> stock, result;
-	// stock += 4,3,4,1,7,8;
-	stock += 7,3,1;
+	Vector<int> stock;
+	stock += 4,3,4,1,7,8;
+	// stock += 4,3,4,8,7,1;
+	// stock += 3,4,3,7;
+	int length = 10;
 
-	cout << canCuted(stock, 10) << endl;
+	cout << "stock: " << stock.toString() << endl;
+	cout << "length: " << length << endl;
 
-	subSetSum(stock, 10, result);
-	cout << result.toString() << endl;
+	cout << "canBeCuted? " << canCuted(stock, length) << endl;
+	cout << "------------------" << endl;
+
+	// cout << subSetSumVecs(stock, length).toString() << endl;
+	// cout << "------------------" << endl;
+
+	cout << CutStock(stock, length) << endl;
+
 
 	return 0;
 }
 
 bool canCuted(Vector<int> & vec, int length) {
-	if (vec.isEmpty()) {
-		return length == 0;
-	} else {
+	if (length < 0) {
+		return false;
+	} else if (vec.isEmpty()) {return length == 0;}
+	else {
 		Vector<int> rest = vec;
 		int element = rest[0];
 		rest.remove(0);
@@ -54,41 +67,82 @@ bool canCuted(Vector<int> & vec, int length) {
 
 }
 
-void subSetSum(Vector<int> & vec, int length, Vector<int> & result) {
+Map<int, Vector<int> > subSetSumVecs(Vector<int> & vec, int length) {
+	Map<int, Vector<int> > result;
+	int index = 0;
 	if (length == 0) {
-		result.add(0);
-		cout << result.toString() << endl;
+		cout << "Finded one subSet" << endl;
+		Vector<int> initVec;
+		initVec += 0;
+		result.put(index, initVec);
+		// cout << result.toString() << endl;
 	} else if (vec.isEmpty() || length < 0) {
-
+		// return result;
 	} else {
 		Vector<int> rest = vec;
 		int element = rest[0];
 		rest.remove(0);
-		for (int i = 0; i < rest.size(); i++) {
-			subSetSum(rest, length - element, result);
-			for(int j = 0; j < result.size(); j++)
-				if (result[j] == 0) result[j] = element;
+		Map<int, Vector<int> > subSets = subSetSumVecs(rest, length - element);
+		// cout << subSet.toString() << endl;
+		foreach(int n in subSets) {
+			Vector<int> subSet = subSets.get(n);
+			if (subSet[0] == 0) subSet.remove(0);
+			subSet += element;
+			result.put(index, subSet);
+			index++;
 		}
+
+		subSets = subSetSumVecs(rest, length);
+		foreach (int n in subSets)  {
+			Vector<int> subSet = subSets.get(n);
+			result.put(index, subSet);
+			index++;
+		}
+		// cout << "---rest, length----" << endl;
+		// cout << subSetSum(rest,length) << endl;
 	}
+	return result;
 }
 
-// int CutStock(Vector<int> & requests, int stockLength) {
-// 	if (stockLength == 0) return count ;
-// 	Vector<int> rest = requests;
-// 	int count = 0;
-// 	Vector<int> subSum = subSetSum(rest, stockLength);
-// 	if (subSum.isEmpty()) {
-// 		return count + CutStock(rest, stockLength -1);
-// 	} else {
-// 		for(int i = 0; i < rest.size(); i++) {
-// 			for (int j = 0 ; j < subSum.size(); j++) {
-// 				if (rest[i] == subSum[j]) {
-// 					subSum.remove(j);
-// 					rest.remove(i);
-// 				}
 
-// 			}
-// 		}
-// 		return 1 + CutStock(rest, stockLength);
+int CutStock(Vector<int> & requests, int stockLength) {
+	int count = 0;
+	if ( stockLength == 0) {
+
+	} else if(requests.isEmpty()) {
+		
+	} else {
+		
+		while (true) {
+			Map<int, Vector<int> > map = subSetSumVecs(requests, stockLength);
+			if (map.isEmpty()) break;
+			Vector<int> tmp = map.get(0);
+			// if (!isSubVec(tmp, requests)) break; 
+			cout << "Find one cuted:" << tmp.toString() << endl;
+			for(int i = 0; i < requests.size(); i++) {
+				for(int j = 0; j < tmp.size(); j++) {
+					if (tmp[j] == requests[i]) {
+						requests.remove(i);
+						tmp.remove(j);
+						j--;
+						// cout << requests.toString() << endl;
+						// cout << tmp.toString() << endl;
+					}
+				}
+			}
+			// if (tmp.isEmpty())
+				count++;
+			// else
+				// break;
+		}
+		return count + CutStock(requests, stockLength - 1);
+	}
+	return count;
+}
+
+// bool isSubVec(Vector<int> subvec, Vector<int> vec) {
+// 	foreach(int n in subvec) {
+
 // 	}
+
 // }
--- Blank Project/SubsetSum.cpp
+++ Blank Project/SubsetSum.cpp
@@ -8,11 +8,13 @@
 #include <string>
 #include "simpio.h"
 #include "vector.h"
+#include "map.h"
 
 using namespace std;
 
 bool CanMakeSum(Vector<int> & nums, int targetSum);
 bool listSubset(Vector<int> & nums, int targetSum);
+Map<int, Vector<int> > subSetSumVecs(Vector<int> & vec, int length, int index);
 
 int main()
 {
@@ -30,13 +32,8 @@ int main()
 	cout << "----------" << endl;
 
 	cout << CanMakeSum(nums, targetSum) << endl;
-	for(int i = 0; i < nums.size(); i++) {
-		int temp = nums[0];
-		nums.remove(0);
-		nums.add(temp);
-		listSubset(nums, targetSum);
-	}
-
+	
+	listSubset(nums, targetSum);
 	
 	return 0;
 }
@@ -55,27 +52,42 @@ bool CanMakeSum(Vector<int> &nums, int targetSum) {
 	}
 }
 
-bool listSubset(Vector<int> & nums, int targetSum) {
-	
+void listSubset(Vector<int> & nums, int targetSum) {
+	subSetSumVecs(nums, targetSum, 0).toString();
+}
 
-	if (targetSum == 0) {
-		cout << "Finded!" << endl;
-		return true;
+Map<int, Vector<int> > subSetSumVecs(Vector<int> & vec, int length, int index) {
+	Map<int, Vector<int> > result;
+	if (length == 0) {
+		cout << "Finded,index: " << index << endl;
+		Vector<int> initVec;
+		initVec += 0;
+		result.put(index, initVec);
+		// cout << result.toString() << endl;
+	} else if (vec.isEmpty() || length < 0) {
+		// return result;
 	} else {
-			if(nums.isEmpty()) return false;
-		// for (int i = 0 ; i < nums.size(); i++) {
-			// int element = nums[i];
-			int element = nums[0];
-			Vector<int> rest = nums;
-			// rest.remove(i);
-			rest.remove(0);
-			if (listSubset(rest, targetSum - element)) {
-				cout << element << ", ";
-				return true;
-			}
-			if (listSubset(rest, targetSum)) return true; 
-			return false;
-		// }		
-	}
+		Vector<int> rest = vec;
+		int element = rest[0];
+		rest.remove(0);
+		Map<int, Vector<int> > subSets = subSetSumVecs(rest, length - element, index);
+		// cout << subSet.toString() << endl;
+		foreach(int n in subSets) {
+			Vector<int> subSet = subSets.get(n);
+			if (subSet[0] == 0) subSet.remove(0);
+			subSet += element;
+			result.put(index, subSet);
+			index++;
+		}
 
+		subSets = subSetSumVecs(rest, length, index);
+		foreach (int n in subSets)  {
+			Vector<int> subSet = subSets.get(n);
+			result.put(index, subSet);
+			index++;
+		}
+		// cout << "---rest, length----" << endl;
+		// cout << subSetSum(rest,length) << endl;
+	}
+	return result;
 }
